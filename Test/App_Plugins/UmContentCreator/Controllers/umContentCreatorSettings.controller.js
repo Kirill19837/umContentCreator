1dfa58fac4375683c7f5c59a1a77b5d0aa1ee173
Mykola Stepanenko <ironfack78@gmail.com>
2023-07-03 09:56:24 +0300

    Revert "remove Test project"
    This reverts commit df92e45f87eb7ac49d72b55e5e100666151c32ea.

---
+++ Test/App_Plugins/UmContentCreator/Controllers/umContentCreatorSettings.controller.js
@@ -0,0 +1,39 @@
+﻿angular.module("umbraco").controller("UmContentCreatorSettingsController",
+    function ($scope, $http, notificationsService) {
+        const saveUrl = "/Umbraco/Api/Configuration/SaveSettings";
+        const loadUrl = "/Umbraco/Api/Configuration/LoadSettings";
+        $scope.model = {
+            apiKey: ''
+        };
+
+        $scope.passwordInputType = 'password';
+        $scope.passwordButtonLabel = 'Show';
+
+        $scope.togglePasswordVisibility = function () {
+            if ($scope.passwordInputType === 'password') {
+                $scope.passwordInputType = 'text';
+                $scope.passwordButtonLabel = 'Hide';
+            } else {
+                $scope.passwordInputType = 'password';
+                $scope.passwordButtonLabel = 'Show';
+            }
+        };
+        
+        $scope.init = function () {
+            $http.get(loadUrl).then(function (response) {
+                $scope.model = response.data;
+            }).catch(function () {
+                notificationsService.error("Error", "Failed to load settings.");
+            });
+        };
+
+        $scope.save = function () {
+            $http.post(saveUrl, $scope.model).then(function () {
+                notificationsService.success("Success", "Settings saved successfully.");
+            }).catch(function () {
+                notificationsService.error("Error", "Failed to save settings.");
+            });
+        };
+
+        $scope.init();
+    });
